ed8cfe613fc0f5b65cb6d22f7cbdf5a824d8f219
Valorad <wcxaaa@gmail.com>
2017-08-31 00:06:47 +0800

    + learned data and method   also learned "this" ambiguity in methods under circumstances of arrow function and normal function.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
-new Vue({
-    el: "#appEntry",
-    data: {
-        gdh: "高端黑 Gaoduanhei"
+"use strict";
+
+const vueData = {
+  gdh: "高端黑 Gaoduanhei",
+  caster: "Steve"
+}
+
+const appEntry = new Vue({
+  el: "#appEntry",
+  data: vueData,
+  methods: {
+    darnify: (target) => {
+      // console.log(this); // window
+      return `Boom!${target} has been blackend by ${vueData.caster}`;
     }
+  }
 });
