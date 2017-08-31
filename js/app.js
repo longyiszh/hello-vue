f525ba3a877719d36fa236f53301d324a9cf30c9
Valorad <wcxaaa@gmail.com>
2017-08-31 11:02:11 +0800

    + learned data and event bindings

--- js/app.js
+++ js/app.js
@@ -2,8 +2,19 @@
 
 const vueData = {
   gdh: "高端黑 Gaoduanhei",
-  caster: "Steve"
-}
+  caster: "Steve",
+  site: "http://www.linruotian.com",
+  html: "<a href='http://www.linruotian.com'>Collect BDK</a>"
+};
+
+const eventData = {
+  bdk: 0,
+  mdk: 0,
+  sdk: 0,
+
+  x: 0,
+  y: 0
+};
 
 const appEntry = new Vue({
   el: "#appEntry",
@@ -12,6 +23,25 @@ const appEntry = new Vue({
     darnify: (target) => {
       // console.log(this); // window
       return `Boom!${target} has been blackend by ${vueData.caster}`;
+    }
+  }
+});
+
+const eventBinding = new Vue({
+  el: "#eventBinding",
+  data: eventData,
+  methods: {
+    change: (dk, op, delta=1) => {
+      if (op === "+") {
+        eventData[dk]++;
+      }
+      else if (op === "-") {
+        eventData[dk]--;
+      }
+    },
+    updateXY: (e) => {
+      eventData.x = e.offsetX,
+      eventData.y = e.offsetY
     }
   }
 });
