4e5af3f5a0c094006c32a92319da918cfb7e9792
Valorad <wcxaaa@gmail.com>
2017-09-07 12:54:43 +0800

    + learned vue component interaction - custom event   * also update webpack to v3 to see what would happen

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,22 @@
       <p>Helping wcxaaa learn vue</p>
       <small>{{ sayHello("wcxaaa") }}</small>
       <cdk-war v-bind:cdkwar="cdkWarData.cdkwar"></cdk-war>
+      
+    <section class="warzone">
+      <h2>WarZone [component interaction - props & vue-custom-event]</h2>
+      <enemy-zone v-bind:bHouse="warZone.bHouse" v-on:bFire="warZone.sHouse.status=$event"></enemy-zone>
+      <h1 class="vs">VS</h1>
+      <div class="factS">
+        <p><span class="faction">SHouse</span></p>
+        <p><span>{{ warZone.sHouse.status }}</span></p>
+        <div class="actionBar">
+          <h3>Actions</h3>
+          <p><button @click="repair()">Repair</button></p>
+          <p><button @click="fireBHouse()">fire XMP to BHouse</button></p>
+        </div>
+      </div>
+    </section>
+
     </main>
   </div>
   <!-- 只能写一个div，没有第二个 -->
@@ -14,6 +30,7 @@
 
 <script>
 import cdkWar from './components/cdkWar.vue';
+import enemyZone from './components/enemyZone.vue'
 
 const cdkWarData = {
   cdkwar: [
@@ -41,20 +58,37 @@ const cdkWarData = {
   ]
 };
 
+const warZone = {
+  sHouse: {
+    status: "active"
+  },
+  bHouse: {
+    status: "active"
+  }
+};
+
 
 export default {
   components: {
-    "cdk-war": cdkWar
+    "cdk-war": cdkWar,
+    "enemy-zone": enemyZone
   },
   data () {
     return {
       title: 'hello-vue',
-      cdkWarData: cdkWarData
+      cdkWarData: cdkWarData,
+      warZone: warZone
     }
   },
   methods: {
     sayHello: (target) => {
       return `Welcome ${target} to dig your vue (grave)!`
+    },
+    repair: () => {
+      warZone.sHouse.status = "active";
+    },
+    fireBHouse: () => {
+      warZone.bHouse.status = "bz~ bzz~ Boom!";
     }
   }
 }
@@ -73,6 +107,26 @@ main.title {
     font-weight: bolder;
     color: $red;
   }
+}
+
+section.warzone {
+    border: 1px dashed #5e5e5e;
+    text-align: center;
+}
+
+section.warzone h1.vs {
+    font-size: 3em;
+    font-weight: bolder;
+}
+
+section.warzone span.faction {
+    font-weight: bolder;
+}
+
+section.warzone div.actionBar {
+    width: 50%;
+    margin: 0.5em auto;
+    border: 1px solid crimson;
 }
 
 </style>
---
+++ src/components/enemyZone.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="factB">
+      <p><span class="faction">BHouse</span></p>
+      <p><span>{{ bHouse.status }}</span></p>
+      <div class="actionBar">
+        <h3>Actions</h3>
+        <p><button @click="repair()">Repair</button></p>
+        <p><button @click="fireSHouse()">fire tactical nuke to SHouse</button></p>
+      </div>
+    </div>
+</template>
+
+<script>
+
+
+
+export default {
+  props: {
+    bHouse: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    repair() {
+      this.bHouse.status = "active"
+    },
+    fireSHouse() {
+      this.$emit('bFire', "Boommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm~")
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
