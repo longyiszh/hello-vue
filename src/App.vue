c5b3a8b795d3f5bb32b348776e26477e1a73d425
Valorad <wcxaaa@gmail.com>
2017-09-09 16:16:12 +0800

    + learned slot

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,124 @@
       <small>{{ sayHello("wcxaaa") }}</small>
       <cdk-war v-bind:cdkwar="cdkWarData.cdkwar"></cdk-war>
       
-    <section class="warzone">
-      <h2>WarZone [component interaction - props & vue-custom-event]</h2>
-      <h3>(event-bus, life-cycle-hook)</h3>
+      <section class="warzone">
+        <h2>WarZone [component interaction - props & vue-custom-event]</h2>
+        <h3>(event-bus, life-cycle-hook)</h3>
 
-      <enemy-zone v-bind:bHouse="warZone.bHouse"></enemy-zone> 
-      <!--  v-on:bFire="warZone.sHouse.status=$event" is used when enemyZone directly fires that event -->
+        <enemy-zone v-bind:bHouse="warZone.bHouse"></enemy-zone> 
+        <!--  v-on:bFire="warZone.sHouse.status=$event" is used when enemyZone directly fires that event -->
 
-      <h1 class="vs">VS</h1>
-      <div class="factS">
-        <p><span class="faction">SHouse</span></p>
-        <p><span>{{ warZone.sHouse.status }}</span></p>
-        <div class="actionBar">
-          <h3>Actions</h3>
-          <p><button @click="repair()">Repair</button></p>
-          <p><button @click="fireBHouse()">fire XMP to BHouse</button></p>
+        <h1 class="vs">VS</h1>
+        <div class="factS">
+          <p><span class="faction">SHouse</span></p>
+          <p><span>{{ warZone.sHouse.status }}</span></p>
+          <div class="actionBar">
+            <h3>Actions</h3>
+            <p><button @click="repair()">Repair</button></p>
+            <p><button @click="fireBHouse()">fire XMP to BHouse</button></p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <section>
+        <div class="actionBar">
+          <button @click="changeIndex(-1)" v-bind:disabled="foodP.currentIndex <= 0"> &lt; </button>
+          <span><b>{{ foodP.guests[foodP.currentIndex].name }}</b>'s Show time</span>
+          <button @click="changeIndex(+1)" v-bind:disabled="foodP.currentIndex >= foodP.guests.length - 1"> &gt; </button>
+        </div>
+
+        <food-present v-if="foodP.currentIndex == 0">
+          <h2 slot="title"> Jianjiaojiao's favorite Food presentation </h2>
+          <h3> [2017-09-09 15:11:36] Bi~~~ hacked by 饼干猫 </h3>
+          <div slot="table" class="table">
+            <ul>
+              <li v-bind:key="food" v-for="food of foodP.guests[0].food">
+                <div class="jjj">
+                  {{ food }}
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div slot="commentArea">
+            <blockquote>不准留言！谁说话我叮谁！</blockquote>
+          </div>
+        </food-present>
+
+        <food-present v-if="foodP.currentIndex == 1">
+          <h2 slot="title"> 饼干猫's most frequent chosen food </h2>
+          <h3> [2017-09-09 15:12:15] Bi~~~ hacked by jianjiaojiao </h3>
+          <h3> [2017-09-09 15:12:16] Bi~~~ hacked by 饼干猫 </h3>
+          <h3> [2017-09-09 15:12:20] Bi~~~ hacked by jianjiaojiao </h3>
+          <h3> ... </h3>
+          <h3> [2017-09-09 15:13:03] Bi~~~ hacked by jianjiaojiao </h3>
+          <h3> [2017-09-09 15:13:04] Bi~~~ hacked by jianjiaojiao </h3>
+          <h3> [2017-09-09 15:13:08] Bi~~~ hacked by 饼干猫 </h3>
+          <h3> [2017-09-09 15:13:08] Bi~~~ hacked by 饼干猫 </h3>
+          <h3> [2017-09-09 15:13:08] <span style="color: red">locked</span> by 饼干猫 </h3>
+          <div slot="table" class="table">
+            <ul>
+              <li v-bind:key="food" v-for="food of foodP.guests[1].food">
+                <div class="bgm">
+                  {{ food }}
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div slot="commentArea">
+            <h3>Meowwwwwwwwwwwwwwwww！</h3>
+            <blockquote>Meowwwwwwwwwwwwwwwww！</blockquote>
+            <blockquote>喵喵喵！</blockquote>
+          </div>
+        </food-present>
+
+        <food-present v-if="foodP.currentIndex == 2">
+          <h2 slot="title"> wc最喜欢吃： </h2>
+          <h3> [2017-09-09 15:12:15] <span style="color: red">[ERR] 饼干猫被jianjiaojiao叮烂了！</span> </h3>
+          <div slot="table" class="table">
+            <ul>
+              <li v-bind:key="food" v-for="food of foodP.guests[2].food">
+                <div class="wc">
+                  {{ food }}
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div slot="commentArea">
+            <div>
+              <table class="commentTable">
+                <thead>
+                  <tr>
+                    <td>Date</td>
+                    <td>Name</td>
+                    <td>Content</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-bind:key="comment" v-for="comment of foodP.pushedComments">
+                    <td>{{ comment.date }}</td>
+                    <td>{{ comment.username }}</td>
+                    <td>{{ comment.comment }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <footer>
+              <form>
+                <input type="text" placeHolder="名字" name="userName" v-model="foodP.userName" required>
+                <input type="text" placeHolder="吐槽" name="comment" v-model="foodP.comment" required>
+                <button @click.prevent="submitContent()">Submit</button>
+              </form>
+            </footer>
+          </div>
+        </food-present>
+
+      <h1>slot 总结一下！</h1>
+      <p>有slot的组件相当于一个框架，由其他开发人员往slot里面扔自定义的html</p>
+      <p>slot 由name名字来区分，如果没有在slot上写名字，那么就是饼干猫hack的地方。</p>
+      <p>数据及函数操作由父组件负责，样式由slot那个组件负责</p>
+      
+
+      </section>
 
     </main>
   </div>
@@ -35,11 +135,13 @@
 <script>
 import cdkWar from './components/cdkWar.vue';
 import enemyZone from './components/enemyZone.vue';
+import foodPresent from './components/foodPresent.vue'
 
 import { busA } from './busA';
 
 
 const cdkWarData = {
+  
   cdkwar: [
     {
       name: 'Billy',
@@ -74,11 +176,38 @@ const warZone = {
   }
 };
 
+const foodP = {
+  currentIndex: 0,
+  guests: [
+    {
+      name: "jianjiaojiao",
+      food: ["cotton", "ruaruaChong", "jjjFood"]
+    },
+    {
+      name: "BingGanMao",
+      food: ["皇家猫粮", "雪域小馒头", "进口奥利奥"]
+    },
+    {
+      name: "wc",
+      food: ["pepper", "salad", "burger"]
+    }
+  ],
+  userName: "",
+  comment: "怒戳楼上！",
+  pushedComments: [
+    {
+      username: "jianjiaojiao",
+      date: new Date("2017-09-09"),
+      comment: "原来你喜欢吃我叮剩下的啊～"
+    }
+  ]
+}
 
 export default {
   components: {
     "cdk-war": cdkWar,
-    "enemy-zone": enemyZone
+    "enemy-zone": enemyZone,
+    "food-present" : foodPresent
   },
   created() {
     busA.$on("bFire", (data) => {
@@ -89,7 +218,8 @@ export default {
     return {
       title: 'hello-vue',
       cdkWarData: cdkWarData,
-      warZone: warZone
+      warZone: warZone,
+      foodP: foodP
     }
   },
   methods: {
@@ -101,7 +231,32 @@ export default {
     },
     fireBHouse: () => {
       warZone.bHouse.status = "bz~ bzz~ Boom!";
+    },
+
+    // findGuest: (guestName) => {
+    //   for (let guest of foodP.guests) {
+    //     if (guest.name === guestName) {
+    //       return guest;
+    //     }
+    //   }
+    //   return null;
+    // },
+    changeIndex: (num) => {
+      let indexAfter = foodP.currentIndex + num;
+      if (indexAfter >= 0 && indexAfter < foodP.guests.length) {
+        foodP.currentIndex += num;
+      }
+    },
+    submitContent: () => {
+      if (foodP.userName.length > 0 && foodP.comment.length > 0) {
+        foodP.pushedComments.push({
+          username: foodP.userName,
+          date: new Date(),
+          comment: foodP.comment
+        });
+      }
     }
+
   }
 }
 </script>
---
+++ src/components/foodPresent.vue
@@ -0,0 +1,73 @@
+<template>
+  <section>
+
+    <slot name="title"></slot>
+
+    <h3>Food Table [slot]</h3>
+
+    <slot></slot>
+
+    <div class="bigTable">
+      <slot name="table"></slot>
+    </div>
+    
+    <div class="commentArea">
+      <slot name="commentArea"></slot>
+    </div>
+    
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+      return {}
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  div.bigTable {
+    border: 1px dashed indigo;
+  }
+  div.table {
+    text-align: center;
+    ul {
+      display: inline-block;
+      list-style-type: none;
+      padding-left: 0px;
+    }
+    div.jjj {
+      width: 200px; border: 1px solid cyan
+    }
+    div.bgm {
+      width: 300px;
+      border: 2px solid gold;
+      background-color: #fafafa;
+      box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16),
+                  0 0 0 1px rgba(0,0,0,0.08);
+      border-radius: 2px;
+    }
+    div.wc {
+      width: 10em;
+      border: 2px solid crimson;
+      border-radius: 2px;
+      margin: 1em;
+    }
+  }
+
+  div.commentArea {
+    border: 2px solid darkkhaki;
+    background-color: #fafafa;
+
+    table.commentTable {
+      margin: 0px auto;
+    }
+
+  }
+
+</style>
+
