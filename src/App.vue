505d621c7c5aa1d74baee658016f95ec1fc29260
Valorad <wcxaaa@gmail.com>
2017-09-14 13:40:22 +0800

    * 美化了一下导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
-  <div class="root" v-wmode:report="{width: 'narrow'}">
-
-    <main class="title">
-
-      <nav class="appNav">
-        <h3>app nav</h3>
-        <ul>
-          <li><router-link to="/index">index</router-link></li>
-          <li><router-link to="/cdkwar">cdkwar</router-link></li>
-          <li><router-link to="/warzone">warZone</router-link></li>
-          <li><router-link to="/foodpresent">foodpresent</router-link></li>
-          <li><router-link to="/application">application</router-link></li>
-          <li><router-link to="/pipe">pipe</router-link></li>
-          <li><router-link to="/mixin">mixin</router-link></li>
-        </ul>
-      </nav>
+  <div class="root">
+    <nav class="appNav">
+      <ul>
+        <li><router-link to="/index">index</router-link></li>
+        <li><router-link to="/cdkwar">cdkwar</router-link></li>
+        <li><router-link to="/warzone">warZone</router-link></li>
+        <li><router-link to="/foodpresent">foodpresent</router-link></li>
+        <li><router-link to="/application">application</router-link></li>
+        <li><router-link to="/pipe">pipe</router-link></li>
+        <li><router-link to="/mixin">mixin</router-link></li>
+      </ul>
+    </nav>
+    <main class="title" v-wmode:report="{width: 'narrow'}">
 
       <div class="router">
         <router-view></router-view>
@@ -42,18 +39,36 @@ export default {
 <style lang="scss">
 
   div.root {
-    margin: 0px auto;
+    
   }
 
   main.title {
     text-align: center;
+    margin: 0px auto;
   }
 
   nav.appNav {
+    background-color: #24292e;
+    height: 4em;
+    padding: 0.125em;
+    // width: 122%;
+    // margin-left: -100%;
+    // padding-left: 100%;
     ul {
       list-style-type: none;
       li {
         display: inline;
+        margin: 1em 2em;
+        a {
+          padding: 0.6em 0.5em;
+          font-size: 1.5em;
+          color: #eeeeee;
+          text-decoration: none;
+        }
+        a.router-link-exact-active {
+          color: white;
+          background-color: #455a64;
+        }
       }
     }
   }
